0e7859459c314f67f6df34265654514461b09785
Gustavo Souza <gustavoelia7@gmail.com>
2025-02-24 12:12:23 -0300

    sass setup, main components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { PomodoroComponent } from './pomodoro/pomodoro.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    TodoListComponent,
+    PomodoroComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'pomodoro-timer';
-}
+export class AppComponent {}
