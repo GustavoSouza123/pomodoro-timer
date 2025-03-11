673bf58986717368542958e51cb78775ccbba8f4
Gustavo Souza <gustavoelia7@gmail.com>
2025-03-11 19:43:53 -0300

    move header component to home component (router outlet)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TodoListComponent } from '../todo-list/todo-list.component';
 import { PomodoroComponent } from '../pomodoro/pomodoro.component';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [TodoListComponent, PomodoroComponent],
+  imports: [HeaderComponent, TodoListComponent, PomodoroComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
