46ce308010e7b3dcba49f7cf46876134fcdaf054
Gustavo Souza <gustavoelia7@gmail.com>
2025-03-12 14:44:52 -0300

    error route with static data

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,14 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'not-found',
+    component: ErrorPageComponent,
+    data: { message: 'Page not found' },
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found',
   },
 ];
